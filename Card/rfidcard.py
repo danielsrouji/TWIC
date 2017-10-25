ca57302af06eec02d3ba81c4cb362695842ee55d
Danny <sroujidaniel@gmail.com>
2017-10-25 17:52:43 +0000

    Successfully posting information to database

--- Card/rfidcard.py
+++ Card/rfidcard.py
@@ -1,12 +1,41 @@
 import serial
-serial = serial.Serial("/dev/ttyUSB1", baudrate=9600)
+import requests
+import json
+serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
 
-code = ''
+card1 = '18004865AB9E'
+card2 = '19007E5E4970'
 
-while True:
-        data = serial.read()
-        if data == '\r':
-                print(code)
-                code = ''
-        else:
-                code = code + data
+
+
+'''
+Data is read from the card as individual symbols/variables
+'''
+firebase_url = 'https://twic-polymer.firebaseio.com/'
+
+def post_to_db(card_id):
+    data={'id':card_id, 'number of times tapped': 0}
+    sent = json.dumps(data)
+    result = requests.post(firebase_url+'/'+card_id+'/cards.json', sent)
+    print 'Success!' + str(result.status_code) + ',' + result.text
+
+
+def check_card(code):
+    if code.find(card1) > 0:
+        print('victory')
+    else:
+        print('failure')
+
+def read_card():
+    code = ''
+    while True:
+            data = serial.read()
+            if data == '\r':
+                    print(code)
+                    check_card(code)
+                    post_to_db(card1)
+                    code = ''
+            else:
+                    #print('testing validation')
+                    code = code + data
+read_card()
